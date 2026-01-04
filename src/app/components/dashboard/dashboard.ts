0c0943f4f019e792a993f19893a5a271be5dfc97
aarthinovem14-cloud <aarthinovem14@gmail.com>
2026-01-04 20:16:09 +0530

    Updated dashboard layout

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
